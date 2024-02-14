2d764ef6a6450321560909b7b5a3f833f3272a5b
Alan Rivera <arivera@bsd220.org>
2024-02-14 09:39:54 -0600

    1st day

---
+++ Assignment Notebook 2/AddAssignment.swift
@@ -0,0 +1,7 @@
+//
+//  AddAssignment.swift
+//  Assignment Notebook 2
+//
+//  Created by Alan Rivera on 2/14/24.
+//
+
--- Assignment Notebook 2/ContentView.swift
+++ Assignment Notebook 2/ContentView.swift
@@ -6,21 +6,57 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @ObservedObject var assignmentList = AssignmentList()
+    @State private var showingAddAssignmentView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) { item in
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(item.dueDate, style: .date)
+                        }
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 8)
+                        .background(Color.red)
+                        .border(Color.black, width: 2)
+                        .cornerRadius(7)
+                        Text(item.description)
+                            .padding(.leading, 8)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 4)
+                            .background(Color.red)
+                            .border(Color.black, width: 2)
+                            .cornerRadius(7)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    assignmentList.items.remove(atOffsets: indexSet)
+                }
+            }
+            .sheet(isPresented: $showingAddAssignmentView) {
+                AddAssignment(assignmentList: assignmentList)
+            }
+            .navigationBarTitle("Assignments", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: { showingAddAssignmentView = true }) {
+                Image(systemName: "plus")
+            })
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+struct AssignmentItem: Identifiable, Codable {
+    var id = UUID()
+    var description = String()
+    var dueDate = Date()
+}
+
