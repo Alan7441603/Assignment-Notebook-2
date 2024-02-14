cd97638c3d737eefde11d58fd564726ea0cb3b7d
Alan Rivera <arivera@bsd220.org>
2024-02-14 09:33:19 -0600

    Initial Commit

---
+++ Assignment Notebook 2/Assignment_Notebook_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_2App.swift
+//  Assignment Notebook 2
+//
+//  Created by Alan Rivera on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
